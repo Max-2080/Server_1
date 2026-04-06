a732d57b1a1eb5d3ac6eb47d5596ad3d18f6ee08
Max <sv.maksim_2006@mail.ru>
2026-04-06 10:26:08 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Otw struct {
+	Result string
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/start", func(c *fiber.Ctx) error {
+
+		return c.SendString("Добро пожаловать в моё приложение FastAPI!")
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+
+		return c.SendFile("index.html")
+	})
+
+	app.Get("/calculate", func(c *fiber.Ctx) error {
+		num1 := c.Query("num1", "")
+		num2 := c.Query("num2", "")
+		num3, _ := strconv.Atoi(num1)
+		num4, _ := strconv.Atoi(num2)
+		num3 += num4
+		num5 := strconv.Itoa(num3)
+		otw := Otw{Result: num5}
+		return c.JSON(otw)
+	})
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		ot := User{Id: 0, Name: "qwerty"}
+		return c.JSON(ot)
+	})
+
+	app.Listen(":3000")
+}
